ff2e273c61c60a7756e35c937c0278b9c79373cb
Darwin <darwindopao@gmail.com>
2026-06-03 15:11:27 -0300

    Fix: conflict in the status database

---
+++ MidasAPI/Migrations/20260603181048_Fix_Status_DATABASE.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MidasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Status_DATABASE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
